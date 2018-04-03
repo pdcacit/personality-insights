476804534ffe39c7e69479cfefbf0682c571027a
Fatima Hiprenoridel Hizon <fhchizon@gmail.com>
2018-04-03 14:15:09 +0800

    First commit

---
+++ src/main/java/com/acn/acit/watsonservice/personalityinsights/PersonalityInsightsDemo.java
@@ -0,0 +1,23 @@
+package com.acn.acit.watsonservice.personalityinsights;
+
+
+public class PersonalityInsightsDemo {
+	/*	
+	 * Please refer on Personality_Insights_HandsOn.ppt
+	 * Visit API Reference https://www.ibm.com/watson/developercloud/personality-insights/api/v3/
+	 * Copy Maven dependency in pom.xml,
+	 * Update the pom dependency: Right click on project > Maven > Update Project
+	 * On API Reference: Click on JAVA tab.
+	 * On API Reference: Click on Methods > Get profile
+	 * Copy and paste the code inside public static void main(String[] args){}
+	 * Import necessary libraries
+	 * Copy and change the version, username, password based on your personality-insights service credentials.
+	 * To run using in eclipse: Right click on the project in eclipse > Run As > Java Application
+	 * To run using command prompt: Go to the project directory and type the command > 'mvn exec:java'
+	 * To rebuild type 'mvn package' or 'mvn verify' before executing 'mvn exec:java' command
+	*/
+	public static void main(String[] args) {
+		//Paste code here...
+	}
+
+}
